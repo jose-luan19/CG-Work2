6a5b527578c7ad93a4a81b9641fc55bc5c4fa77a
joseluan620@gmail.com <joseluan620@gmail.com>
2024-08-27 19:21:29 -0300

    correct q3 and q4 for use POO

--- q4.py
+++ q4.py
@@ -1,141 +1,24 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import q3
+import utils.utils as util
 
-
-def plotaSolido2D(ax, pontos, arestas, color="g"):
-    for aresta in arestas:
-        x = [pontos[0][aresta[0]], pontos[0][aresta[1]]]
-        y = [pontos[1][aresta[0]], pontos[1][aresta[1]]]
-        ax.plot(x, y, color)
-
-
-cubeVertexListCamera = q3.cubeVertexListCamera.T
-cilindroVertexListCamera = q3.cilindroVertexListCamera.T
-coneVertexListCamera = q3.coneVertexListCamera.T
-esferaVertexListCamera = q3.esferaVertexListCamera.T
-troncoVertexListCamera = q3.troncoVertexListCamera.T
-
-near = 0.1  # Valor de near
-far = 100.0  # Valor de far
-
-
-def TransformacaoEmPerspectiva(vertices, alpha, near, far):
-    matrizHomogenea = np.vstack((vertices, np.ones((1, vertices.shape[1]))))
-    verticesEmPerspectiva = np.zeros_like(matrizHomogenea)
-
-    for i in range(matrizHomogenea.shape[1]):
-        verticesEmPerspectiva[:, i] = (
-            np.array(
-                [
-                    [1 / (matrizHomogenea[2, i] * np.tan(alpha / 2)), 0, 0, 0],
-                    [0, 1 / (matrizHomogenea[2, i] * np.tan(alpha / 2)), 0, 0],
-                    [
-                        0,
-                        0,
-                        (near + far) / (near - far),
-                        (2 * near * far) / (near - far),
-                    ],
-                    [0, 0, -1, 0],
-                ]
-            )
-            @ matrizHomogenea[:, i]
-        )
-
-    verticesEmPerspectiva = verticesEmPerspectiva[:-1, :]
-    verticesEmPerspectiva[-1, :] = 0
-
-    return verticesEmPerspectiva
-
-
-# Exemplo de matriz de vértices 3D
-vertices_cubo = np.array(cubeVertexListCamera)
-vertices_cone = np.array(coneVertexListCamera)
-vertices_cilindro = np.array(cilindroVertexListCamera)
-vertices_tronco = np.array(troncoVertexListCamera)
-vertices_esfera = np.array(esferaVertexListCamera)
-
-# Ângulo alpha em radianos
-alpha = np.pi / 2
-
-# Chamada da função de transformação em perspectiva
-vertices_transformados_cubo = TransformacaoEmPerspectiva(
-    vertices_cubo, alpha, near, far
-)
-# Chamada da função de transformação em perspectiva
-vertices_transformados_cone = TransformacaoEmPerspectiva(
-    vertices_cone, alpha, near, far
-)
-# Chamada da função de transformação em perspectiva
-vertices_transformados_cilindro = TransformacaoEmPerspectiva(
-    vertices_cilindro, alpha, near, far
-)
-# Chamada da função de transformação em perspectiva
-vertices_transformados_tronco = TransformacaoEmPerspectiva(
-    vertices_tronco, alpha, near, far
-)
-# Chamada da função de transformação em perspectiva
-vertices_transformados_esfera = TransformacaoEmPerspectiva(
-    vertices_esfera, alpha, near, far
-)
-
-# Criação do plot 2D
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-# Criação do plot 3D
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection="3d")
-
-# Plota cubo original
-# q3.plotaSolido(ax, cubeVertexListCamera, q3.cubo.arestas)
-
-# Plota cubo projetado
-plotaSolido2D(ax, vertices_transformados_cubo, q3.cubo.arestas)
-# q3.plotaSolido(ax, vertices_transformados_cubo, q3.cubo.arestas)
-
-# Plota cilindro original
-# q3.plotaSolido(ax, cilindroVertexListCamera, q3.cilindro.arestas, "b")
-
-# Plota cilindro projetado
-plotaSolido2D(ax, vertices_transformados_cilindro, q3.cilindro.arestas, "b")
-# q3.plotaSolido(ax, vertices_transformados_cilindro, q3.cilindro.arestas, "b")
-
-# Plota tronco original
-# q3.plotaSolido(ax, troncoVertexListCamera, q3.tronco.arestas, "c")
-
-# Plota tronco projetado
-plotaSolido2D(ax, vertices_transformados_tronco, q3.tronco.arestas, "c")
-# q3.plotaSolido(ax, vertices_transformados_tronco, q3.tronco.arestas, "c")
-
-# Plota esfera original
-# q3.plotaSolido(ax, esferaVertexListCamera, q3.esfera.arestas, "m")
-
-# Plota esfera projetado
-plotaSolido2D(ax, vertices_transformados_esfera, q3.esfera.arestas, "m")
-# q3.plotaSolido(ax, vertices_transformados_esfera, q3.esfera.arestas, "m")
-
-# Plota tronco original
-# q3.plotaSolido(ax, coneVertexListCamera, q3.cone.arestas, "k")
-
-# Plota tronco projetado
-plotaSolido2D(ax, vertices_transformados_cone, q3.cone.arestas, "k")
-# q3.plotaSolido(ax, vertices_transformados_cone, q3.cone.arestas, "k")
-
-camera = q3.eye
-# Plot da camera no 3d
-# ax.scatter(camera[0], camera[1], camera[2], c="r", label="eye")
-
-at = q3.soma
-# Plot do at no 3d
-# ax.scatter(at[0], at[1], at[2], c="y", label="at")
-
-# Configurações do plot
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-# ax.set_zlabel("Z")
-# ax.legend()
-
-# Exibição do plot
-plt.show()
  
+if __name__ == "__main__":
+    
+    cubo, cone, cilindro, tronco, esfera, _ = q3.main()
+    
+    cubo.transformacao_perspectiva()
+    cone.transformacao_perspectiva()
+    cilindro.transformacao_perspectiva()
+    tronco.transformacao_perspectiva()
+    esfera.transformacao_perspectiva()
+    
+    # Criação da figura e do subplot 3D
+    ax = util.create_figure2D()
+    
+    ax = cubo.plota_solido2D(ax, cor="g")
+    ax = cone.plota_solido2D(ax, cor="k")
+    ax = cilindro.plota_solido2D(ax, cor="b")
+    ax = tronco.plota_solido2D(ax, cor="c")
+    ax = esfera.plota_solido2D(ax, cor="m")
+    
+    util.show_figure2D(ax)
--- utils/utils.py
+++ utils/utils.py
@@ -9,11 +9,24 @@ class Solido(ABC):
   def __init__(self, ponto_inicial):
     self.ponto_inicial = ponto_inicial
     self.pontosX, self.pontosY, self.pontosZ, self.arestas = [], [], [], []
-    
   
+  def get_centro_massa(self):
+    lista_vertices = self.get_vertices_lista().T
+    return np.mean(lista_vertices, axis=0)
+  
+  def get_vertices_lista(self):
+    return np.array([self.pontosX, self.pontosY, self.pontosZ])
+  
+  def set_vertices_lista(self, new_vertices):
+    self.pontosX, self.pontosY, self.pontosZ = new_vertices
+    
   def plota_solido(self, axes: Axes, cor="b") -> Axes:
     # Plota o cone no gráfico 3D.
-    return plotaSolido(self, axes, cor)
+    return plotaSolido(self, axes, cor)    
+  
+  def plota_solido2D(self, axes: Axes, cor="g") -> Axes:
+    # Plota o cone no gráfico 3D.
+    return plotaSolido2D(self, axes, cor)
     
   def escalar_solido(self, sx, sy, sz):
     self.pontosX, self.pontosY, self.pontosZ = escalar(sx, sy, sz, self)
@@ -24,6 +37,54 @@ class Solido(ABC):
   def transladar_solido(self, dx, dy, dz):
     self.pontosX, self.pontosY, self.pontosZ = transladar(dx, dy, dz, self)
   
+  def convertWorldToCamera(self ,U, V, N, eye):
+    RTMatrix = np.array(
+        [
+            [U[0], U[1], U[2], -np.dot(eye, U)],
+            [V[0], V[1], V[2], -np.dot(eye, V)],
+            [N[0], N[1], N[2], -np.dot(eye, N)],
+            [0, 0, 0, 1],
+        ]
+    )
+    lista_vertices = self.get_vertices_lista().T
+    cameraVertexList = np.dot(
+        RTMatrix, np.concatenate([lista_vertices.T, np.ones((1, lista_vertices.shape[0]))])
+    )
+    cameraVertexList = cameraVertexList[:3]
+    self.set_vertices_lista(cameraVertexList)  # isso aqui é o solido transformado (já foi rotacionado e transladado)
+    
+  def transformacao_perspectiva(self):
+    near = 0.1  
+    far = 100.0
+    alpha = np.pi / 2 # Ângulo alpha em radianos
+    lista_vertices = self.get_vertices_lista()
+    
+    matrizHomogenea = np.vstack((lista_vertices, np.ones((1, lista_vertices.shape[1]))))
+    verticesEmPerspectiva = np.zeros_like(matrizHomogenea)
+
+    for i in range(matrizHomogenea.shape[1]):
+      verticesEmPerspectiva[:, i] = (
+        np.array(
+          [
+            [1 / (matrizHomogenea[2, i] * np.tan(alpha / 2)), 0, 0, 0],
+            [0, 1 / (matrizHomogenea[2, i] * np.tan(alpha / 2)), 0, 0],
+            [
+              0,
+              0,
+              (near + far) / (near - far),
+              (2 * near * far) / (near - far),
+            ],
+            [0, 0, -1, 0],
+          ]
+        )
+        @ matrizHomogenea[:, i]
+      )
+
+    verticesEmPerspectiva = verticesEmPerspectiva[:-1, :]
+    verticesEmPerspectiva[-1, :] = 0
+    self.set_vertices_lista(verticesEmPerspectiva)
+    
+    
   @abstractmethod
   def gerar_coordenadas(self):
     pass 
@@ -32,12 +93,37 @@ class Solido(ABC):
   def gerar_solido(self):
     pass
   
+  
+def calcular_vetores_aux(media_solidos, eye):
+  # Calculando os vetores N, U e V da câmera
+  at = np.array(media_solidos)
+  n = at - eye
+  aux = np.array([0, 1, 0])
+  v = np.cross(aux, n)
+  u = np.cross(v, n)
+  N = n / np.linalg.norm(n)
+  V = v / np.linalg.norm(v)
+  U = u / np.linalg.norm(u)
+  return U, V, N
+  
 def create_figure() -> Axes:
   fig = plt.figure()
   axes = fig.add_subplot(111, projection="3d")
+  return axes  
+
+def create_figure2D() -> Axes:
+  fig = plt.figure()
+  axes = fig.add_subplot(111)
   return axes
 
-
+def calcular_media_solidos(cubeCenterMass, coneCenterMass, cilindroCenterMass, troncoCenterMass, esferaCenterMass):
+  media = (cubeCenterMass
+    + coneCenterMass
+    + cilindroCenterMass
+    + troncoCenterMass
+    + esferaCenterMass
+  ) / 5
+  return media
 
 def plotaSolido(solido: Solido, axes: Axes, cor) -> Axes:
   # Plota o sólido definido pelos pontos e arestas no gráfico 3D.
@@ -49,11 +135,36 @@ def plotaSolido(solido: Solido, axes: Axes, cor) -> Axes:
     axes.plot(x, y, z, cor)
   return axes
 
+def plotaSolido2D(solido: Solido, axes: Axes, cor):
+  pontos = [solido.pontosX, solido.pontosY]
+
+  for aresta in solido.arestas:
+    x = [pontos[0][aresta[0]], pontos[0][aresta[1]]]
+    y = [pontos[1][aresta[0]], pontos[1][aresta[1]]]
+    axes.plot(x, y, cor)
+  return axes
+
+def include_legend(ax: Axes, eye, media_solidos):
+  # Plot do ponto de vista da câmera
+  ax.scatter(eye[0], eye[1], eye[2], color="r", label="eye")
+
+  # Média dos pontos
+  ax.scatter(media_solidos[0], media_solidos[1], media_solidos[2], color="y", label="at")
+
+  # Ponto Origem do Mundo
+  ax.scatter(0, 0, 0, color="m", label="origem")
+  
+  ax.legend()
 
 def show_figure(axes: Axes):
   axes.set_xlabel("X")
   axes.set_ylabel("Y")
   axes.set_zlabel("Z")
+  plt.show()
+
+def show_figure2D(axes: Axes):
+  axes.set_xlabel("X")
+  axes.set_ylabel("Y")
   plt.show()
 
 def rotacionar(angle_x, angle_y, angle_z, solido: Solido):
@@ -116,3 +227,23 @@ def transladar(dx, dy, dz, solido: Solido):
   y = new_matrix[1]
   z = new_matrix[2]
   return x, y, z
+
+
+def calculateCenterMass(vertexList):
+  return np.mean(vertexList, axis=0)
+
+
+def convertWorldToCamera(vertexList, U, V, N, eye):
+  RTMatrix = np.array(
+    [
+      [U[0], U[1], U[2], -np.dot(eye, U)],
+      [V[0], V[1], V[2], -np.dot(eye, V)],
+      [N[0], N[1], N[2], -np.dot(eye, N)],
+      [0, 0, 0, 1],
+    ]
+  )
+  cameraVertexList = np.dot(
+    RTMatrix, np.concatenate([vertexList.T, np.ones((1, vertexList.shape[0]))])
+  )
+  cameraVertexList = cameraVertexList[:3].T
+  return cameraVertexList  # isso aqui é o solido transformado (já foi rotacionado e transladado)
